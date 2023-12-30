58936dab8df93ad1d5381ba2ce8d8e1a281eea9e
Subhash Parwani <subhashparwani6685@gmail.com>
2023-12-30 21:22:04 +0500

    api details

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import axios from "axios";
 
 function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
+  const [like, setLike] = useState(false);
+  const [saved, setSaved] = useState(false);
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -11,12 +14,26 @@ function Row({ title, fetchURL }) {
   }, [fetchURL]);
 
   console.log(movies);
+
+  const saveShow = async () => {
+    if (user?.email) {
+      setLike(!like);
+      setSaved(true);
+      await updateDoc(movieID, {
+        savedShows: arrayUnion({
+          id: item.id,
+          title: item.title,
+          img: item.backdrop_path,
+        }),
+      });
+    } else {
+      alert("Please log in to save a movie");
+    }
+  };
+
   return (
     <>
       <h1>{title}</h1>
-      {/* <div>
-        
-      </div> */}
       <div className="relative flex items-center group">
         <div
           id={"slider"}
@@ -28,6 +45,18 @@ function Row({ title, fetchURL }) {
                 src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`}
                 alt={item.title}
               />
+              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+                  {item?.title}
+                </p>
+                <p onClick={saveShow}>
+                  {like ? (
+                    <FaHeart className="absolute top-4 left-4 text-gray-300" />
+                  ) : (
+                    <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
+                  )}
+                </p>
+              </div>
             </div>
           ))}
         </div>
